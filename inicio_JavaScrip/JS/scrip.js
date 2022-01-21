d755bbf91d1427d9b95ba87646865c82de60ed05
Angel <redtransistor75@gmail.com>
2022-01-20 22:21:54 -0600

    hello again

---
+++ inicio_JavaScrip/JS/scrip.js
@@ -0,0 +1,61 @@
+var mivariable= `rojo`; //esta ya no se usa
+
+let comillas =`` //alt9+96
+let boton= true;
+let boton2 = false;
+
+const pi=3.14;
+console.log(pi);
+
+for( let i=0; i<10; i++){
+    let nombreArchivo = `Expediente`;
+    console.log(i);
+
+}
+//ciclo for
+for( let i=0;  i<10; i++){
+    //console.log(i);  
+  }
+  let j=0;
+  //ciclo while
+  
+  
+  //Arreglos
+  
+  let shopping = ['bread', 'milk', 'cheese', 'hummus', 'noodles'];
+  
+  for( let i=0;  i<5; i++){
+      //console.log(shopping[i]);  
+  }
+  
+  //JSOn
+  const manzana = {
+      color: 'rojo',
+      marca: 'appleRed',
+      distribucion:{
+          pais: 'Estados UNIDOS',
+          telefono: '557766223'
+      },
+      function(){
+  
+      }
+  }
+  
+  
+  console.log(   manzana.distribucion, manzana.color);
+  
+  
+  while(j<10){
+      if(j==5){
+          //console.log("Efectivamente j es igual a 5");
+      }else{
+          //console.log("j no es 5");
+      } 
+      j++;
+  }
+  //FUNCIONES
+  //FUNTION
+  funtion imprimir(){
+      console.log("esta es mi primer funcon");
+  }
+  imprimir();
